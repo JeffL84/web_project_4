8f0128cfd69656fdf8baadd945079fecb2f1894b
Jeff Lowenhaupt <jeff.lowenhaupt@gmail.com>
2020-05-27 08:01:59 -0400

    fix overlay and media queries

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-
-let formName = document.querySelector(".form__name").value;
-let formOccupation = document.querySelector(".form__description").value;
-
 let form = document.querySelector(".form");
-
-let userName = document.querySelector(".info__name");
-let occupation = document.querySelector(".info__description");
+let userName = document.querySelector(".info__name").innerHTML;
+let occupation = document.querySelector(".info__description").innerHTML;
+let formName = document.querySelector(".form__name");
+let formOccupation = document.querySelector(".form__description");
 
 let editButton = document.querySelector(".info__edit-button");
 let saveButton = document.querySelector(".form__save-button");
 let closeButton = document.querySelector(".form__close-button");
-
-
-closeButton.addEventListener("click", formHide());
-editButton.addEventListener("click", formShow());
 
 
 function formShow() {
@@ -24,11 +17,14 @@ function formHide() {
     form.setAttribute("style", "display: none");
 }
 
-function formSave(){
-   formName = document.querySelector(".form__name").value;
-   formOccupation = document.querySelector(".form__description").value;
-   userName.innerHTML = formName;
-   occupation.innerHTML = formOccupation;
+function formSave(event){
+    event.preventDefault();
+    document.querySelector('.info__name').textContent = formName.value; 
+    document.querySelector('.info__description').textContent = formOccupation.value; 
 
    formHide();
 }
+
+saveButton.addEventListener("click", formSave);
+closeButton.addEventListener("click", formHide);
+editButton.addEventListener("click", formShow);
