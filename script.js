058a5842d1c8a129d514255ad2ad625b31e197b0
Jeff Lowenhaupt <jeff.lowenhaupt@gmail.com>
2020-05-28 22:32:29 -0400

    fix all required errors

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 let form = document.querySelector(".form");
-let userName = document.querySelector(".info__name").innerHTML;
-let occupation = document.querySelector(".info__description").innerHTML;
+//I am not sure I understood your feedback for line 1 - if it is not fixed yet, can you clarify? 
+let userName = document.querySelector(".profile__name").innerHTML;
+let occupation = document.querySelector(".profile__description").innerHTML;
 let formName = document.querySelector(".form__name");
 let formOccupation = document.querySelector(".form__description");
-
-let editButton = document.querySelector(".info__edit-button");
+let editButton = document.querySelector(".profile__edit-button");
 let saveButton = document.querySelector(".form__save-button");
 let closeButton = document.querySelector(".form__close-button");
 
 
 function formShow() {
-    form.setAttribute("style", "display: flex");
+   form.classList.toggle('form'); 
 }
 
 function formHide() {
-    form.setAttribute("style", "display: none");
+    form.classList.toggle('form'); 
 }
 
 function formSave(event){
     event.preventDefault();
-    document.querySelector('.info__name').textContent = formName.value; 
-    document.querySelector('.info__description').textContent = formOccupation.value; 
+    document.querySelector('.profile__name').textContent = formName.value; 
+    document.querySelector('.profile__description').textContent = formOccupation.value; 
 
    formHide();
 }
