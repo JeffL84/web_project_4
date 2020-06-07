9eca206e9cc9fed62e310cd3f640a0a91598f3df
Jeff Lowenhaupt <jeff.lowenhaupt@gmail.com>
2020-06-07 11:28:32 -0400

    code card initialization function

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const saveButton = document.querySelector(".form__save-button");
 const closeButton = document.querySelector(".form__close-button");
 
 
+
 function formToggle() {
    form.classList.toggle('form'); 
 }
@@ -24,3 +25,51 @@ saveButton.addEventListener("click", formToggle);
 closeButton.addEventListener("click", formToggle);
 editButton.addEventListener("click", formToggle);
 
+const initialCards = [
+   {
+      name: "Yosemite Valley",
+      link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+   {
+       name: "Lake Louise",
+       link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+   },
+   {
+       name: "Bald Mountains",
+       link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+   },
+   {
+       name: "Latemar",
+       link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+   },
+   {
+       name: "Vanois National Park",
+       link: "https://code.s3.yandex.net/web-code/vanois.jpg"
+   },
+   {
+       name: "Lago di Braies",
+       link: "https://code.s3.yandex.net/web-code/lago.jpg"
+   }
+];
+
+
+initialCards.forEach((data) => {
+   const cardTemplate = document.querySelector(".elements__template").content.querySelector(".elements__element");
+   console.log(cardTemplate);
+   const cardElement = cardTemplate.cloneNode(true);
+   
+   const cardImage = cardElement.querySelector(".elements__image");
+   console.log(cardImage);
+   const cardTitle = cardElement.querySelector(".elements__name");
+   console.log(cardTitle);
+   const cardLikeButton = cardElement.querySelector(".elements__heart-icon");
+   const cardRemoveButton = cardElement.querySelector(".elements__trash-icon");
+
+   cardTitle.textContent = data.name;
+   cardImage.style.backgroundImage = `url(${data.link})`;
+
+   //add event listeners
+
+   const list = document.querySelector(".elements");
+   list.prepend(cardElement);
+});
