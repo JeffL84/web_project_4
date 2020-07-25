f38bbe6e8ff3af309265baa3f221dd46806dd897
Jeff Lowenhaupt <jeff.lowenhaupt@gmail.com>
2020-07-25 10:08:46 -0400

    add woff in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
         loader: "html-loader",
       },
       {
-        test: /\.(png|svg|jpg|gif|woff2)$/,
+        test: /\.(png|svg|jpg|gif|woff2|woff)$/,
         loader: "file-loader",
       },
     ]
